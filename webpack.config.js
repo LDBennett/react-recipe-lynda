7ce35ea03635d8b8a8c60176548268e3473b2ced
Lee Bennett <LDBennett@gmail.com>
2019-09-19 15:40:17 -0400

    Basic webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  context: path.join(__dirname,'src'),
   entry: './index.js',
   output: {
-    path: __dirname,
+    path: path.resolve(__dirname,'dist'),
     filename: './bundle.js'
   },
   module:{
@@ -14,4 +17,10 @@ module.exports = {
       }
     ]
   },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body'
+    })
+  ]
 };
