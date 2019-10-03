eb022b8e90a6ec84f03b1de53cd28f71bf877094
Lee Bennett <LDBennett@gmail.com>
2019-10-03 11:13:48 -0400

    added routing and HeaderLink

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import Proptypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+
+const HeaderLink = ({ children,...props }) => (
+  <NavLink to="/"
+    exact
+    className="px1 mx2 black rounded text-decoration-none"
+    activeClassName="bg-white"
+    {...props}
+  >
+    {children}
+  </NavLink>
+)
 
 const Header = () => (
-  <header className="px4">
+  <header className="flex items-center justify-between px4">
     <h1 className="h1">MyRecipes</h1>
+    <nav>
+      <HeaderLink to="/">Home</HeaderLink>
+      <HeaderLink to="/favorites">Favorites</HeaderLink>
+    </nav>
   </header>
 );
+
+HeaderLink.propTypes = {
+  children: Proptypes.node,
+}
 
 export default Header;
